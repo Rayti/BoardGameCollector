22f8e2a5a8d45795f7cc84ee2d401b5c27685bcd
Rayti <milosz.karlowicz@gmail.com>
2021-09-28 11:23:37 +0200

    Add GameService

--- app/src/main/java/com/example/boardgamecollector/service/GameService.kt
+++ app/src/main/java/com/example/boardgamecollector/service/GameService.kt
@@ -1,4 +1,49 @@
 package com.example.boardgamecollector.service
 
-class GameService {
+import com.example.boardgamecollector.dao.DataBase
+import com.example.boardgamecollector.model.Game
+import java.util.stream.Collector
+import java.util.stream.Collectors
+
+class GameService(val dataBase: DataBase) {
+
+    fun sortGamesByDate(games: ArrayList<Game>){
+        games.sortWith(compareBy { game -> game.publishedYear})
+    }
+
+    fun sortGamesByRank(games: ArrayList<Game>) {
+        games.sortWith(compareBy { game -> game.rank})
+    }
+
+    fun getStoredGames(): ArrayList<Game> {
+        return dataBase.getGames()
+    }
+
+    fun getStoredGames(location: String): ArrayList<Game> {
+        return dataBase.getGames(location)
+    }
+
+    fun filterGamesByTitle(title: String, games: ArrayList<Game>) {
+        val filteredList = games.stream().filter { game ->
+            val gameTitle = game.title
+            if (gameTitle != null) {
+                return@filter gameTitle.contains(title, true)
+            }
+            false }
+                .collect(Collectors.toList())
+        games.clear()
+        games.addAll(ArrayList(filteredList))
+    }
+
+    fun editStoredGame(game: Game) {
+        dataBase.updateGame(game)
+    }
+
+    fun storeGame(game: Game) {
+        dataBase.addGame(game)
+    }
+
+    fun deleteGame(game: Game) {
+        dataBase.deleteGame(game)
+    }
 }
